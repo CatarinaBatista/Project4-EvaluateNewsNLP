fa14cb823773c8b6b2e55de14720368dd55b20ad
Catarina Batista <catarinabatistayou@gmail.com>
2019-11-29 14:06:47 +0000

    configurations

--- src/server/index.js
+++ src/server/index.js
@@ -1,30 +1,56 @@
 var path = require('path')
-const express = require('express')
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const mockAPIResponse = require('./mockAPI.js')
 var aylien = require("aylien_textapi");
 const dotenv = require('dotenv');
 dotenv.config();
 
+// Aylien engine
 var textapi = new aylien({
     application_id: process.env.API_ID,
     application_key: process.env.API_KEY
 });
 
+
+// Start up an instance of app
 const app = express()
 
+// Initialize the main project folder
 app.use(express.static('dist'))
 
+// Configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Cors for cross origin allowance
+app.use(cors());
+
+
+
 console.log(__dirname)
+
+
+
 
 app.get('/', function (req, res) {
     res.sendFile('dist/index.html')
 })
 
 // designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
-})
+
 
 app.get('/test', function (req, res) {
     res.send(mockAPIResponse)
 })
+
+
+// Setup Server
+const port = 8000;
+
+app.listen(port, listening);
+
+function listening(){
+    console.log(`Running on localhost: ${port}`);
+};
