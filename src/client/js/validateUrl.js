a5dfe775e104ca1f56bb2d35be0b3b0e2b2da9b7
Catarina Batista <catarinabatistayou@gmail.com>
2019-12-02 17:53:01 +0000

    validate url function

---
+++ src/client/js/validateUrl.js
@@ -0,0 +1,10 @@
+function validateUrl(URL) {
+    var regex = /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/;
+    
+    if(regex.test(URL)){
+        return true;
+    }else{
+        return false;
+    }
+}
+export { validateUrl }
