4d9f2185a0ee2cf2fec4b0e6dd7743371a6e20d6
Catarina Batista <catarinabatistayou@gmail.com>
2019-12-02 17:52:48 +0000

    jest test

---
+++ src/client/js/validateUrl.test.js
@@ -0,0 +1,9 @@
+import { validateUrl } from './validateUrl'
+
+test('validate url', () => {
+  expect(validateUrl('https://www.google.com')).toBe(true);
+  expect(validateUrl('https://www.google.com/aricle/text')).toBe(true);
+  expect(validateUrl('http://127.0.0.1')).toBe(true);
+  expect(validateUrl('abcabc')).toBe(false);
+  expect(validateUrl('abcabc.www')).toBe(false);
+});
